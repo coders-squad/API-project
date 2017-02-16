b8377dd969c854e3ec6e7746da78cdbf31834d46
shahenaz Monia <shahy.m.93@gmail.com>
2017-02-16 10:04:52 +0200

    update test.js

--- src.js
+++ src.js
@@ -1,92 +1,96 @@
-var waterfall = function(arg ,cb) {
-  var i = 0;
-  while(i < arg.length){
-    arg[i](function(){
-       waterfall(arg.slice(1),cb)
-    });
-    i++;
-  }
+var waterfall = function(arg) {
+    var i = 0;
+    while (i < arg.length) {
+        arg[i]();
+        i++;
+    }
 }
-
-function submit(){
-setTimeout(function(){
-  document.getElementById("hero").style.display = "block";
-document.getElementById("res").style.display = "none";
-console.log(document.getElementById("search").value);
-var myRequest = new XMLHttpRequest();
-
-
-
- myRequest.onreadystatechange = function() {
-   if (myRequest.readyState === 4 && myRequest.status === 200) {
-       var data = myRequest.responseText;
-       var store = [];
-
-       JSON.parse(data)['hits'].reduce(function(res, elem,idx){
-         store.push(elem['fields']['brand_name']);
-
-      },0);
-
-  var ul =document.getElementById('brands');
-
-  store.forEach(function(ele){
-    var opt =document.createElement("a");
-
-    opt.setAttribute("class", "item");
-    opt.setAttribute("href", "javascript:;");
-
-
-opt.textContent=ele;
+var arr = [];
+function submit() {
+    setTimeout(function() {
+        document.getElementById("hero").style.display = "block";
+        document.getElementById("res").style.display = "none";
+        console.log(document.getElementById("search").value);
+        var myRequest = new XMLHttpRequest();
 
 
 
-ul.appendChild(opt);
-  })
-      // for(var i =0 ;i< store.length; i++){
-      //   var res = "<li><a class=\"item\" href=\"javascript:;\">"+store[i]+"</a></li>";
-      //   console.log(res);
-      //   document.getElementById("brands").innerHTML = res;
-      // }
- }};
+        myRequest.onreadystatechange = function() {
+            if (myRequest.readyState === 4 && myRequest.status === 200) {
+                var data = myRequest.responseText;
+                var store = [];
 
- myRequest.open("GET", "https://api.nutritionix.com/v1_1/search/"+document.getElementById("search").value+"?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=22abaad9&appKey=f286963aed1edb4e49a2b3f179bff498", true);
- myRequest.send();
-},500);
+                JSON.parse(data)['hits'].reduce(function(res, elem, idx) {
+                store.push(elem['fields']['brand_name']);
+                arr.push(elem['fields']);
+                // console.log(elem['fields']);
+                }, 0);
+
+                var div = document.getElementById('brands');
+                div.innerHTML = "";
+                store.forEach(function(ele) {
+                    var opt = document.createElement("a");
+
+                    opt.setAttribute("class", "item");
+                    opt.setAttribute("href", "javascript:;");
+                    opt.setAttribute("value", ele);
+
+                    opt.textContent = ele;
+
+
+
+                    div.appendChild(opt);
+                });
+
+            }
+        };
+
+        myRequest.open("GET", "https://api.nutritionix.com/v1_1/search/" + document.getElementById("search").value + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=22abaad9&appKey=f286963aed1edb4e49a2b3f179bff498", true);
+        myRequest.send();
+    }, 500);
 };
-function functionName() {
-  setTimeout(function(){
-    var cls = document.getElementsByClassName("item");
-    console.log(cls);
-    for(var i=0; i < cls.length;i++){
-      cls[i].onclick = function(){
-        document.getElementById("hero").style.display = "none";
-        document.getElementById("res").style.display = "block";
-      }
-    }
-  },2000);
+
+function getBrands() {
+    setTimeout(function() {
+        var cls = document.getElementsByClassName("item");
+        for (var i = 0; i < cls.length; i++) {
+            
+            cls[i].onclick = function() {
+                document.getElementById("hero").style.display = "none";
+                document.getElementById("res").style.display = "block";
+                var itemVal = this.getAttribute("value");
+                console.log(itemVal);
+                for (var j = 0; j < arr.length; j++) {
+                    if(itemVal == arr[j].brand_name){
+                        document.getElementById("calories").innerHTML = arr[j].nf_calories;
+                        document.getElementById("fats").innerHTML = arr[j].nf_total_fat;
+                    }
+                }
+                
+            }
+        }
+    }, 2000);
 }
 
-function val(){
-setTimeout(function(){
-var myRequest2 = new XMLHttpRequest();
+function getImage() {
+    setTimeout(function() {
+        var myRequest2 = new XMLHttpRequest();
 
-  myRequest2.onreadystatechange = function() {
-    if (myRequest2.readyState === 4 && myRequest2.status === 200) {
-        var data2 = JSON.parse(myRequest2.responseText);
-        var imageUrl = data2.queryExpansions[2].thumbnail.thumbnailUrl;
-        console.log(imageUrl);
-        document.getElementById("meal_img").setAttribute("src",imageUrl);
-         //console.log(data2);
-      // document.getElementById("img1").innerHTML.src = data2['value'][2]['contentUrl'];
-    }
-  };
-  var val = document.getElementById("search").value;
-  myRequest2.open(
-      "POST","https://api.cognitive.microsoft.com/bing/v5.0/images/search?q="+val,
-      true
-  );
-  myRequest2.setRequestHeader("Content-Type","multipart/form-data");
-  myRequest2.setRequestHeader("Ocp-Apim-Subscription-Key","46fa6f32bc44492d87b34c95e1a37a10");
-  myRequest2.send();
-  },3000);
+        myRequest2.onreadystatechange = function() {
+            if (myRequest2.readyState === 4 && myRequest2.status === 200) {
+                var data2 = JSON.parse(myRequest2.responseText);
+                var imageUrl = data2.queryExpansions[2].thumbnail.thumbnailUrl;
+                console.log(imageUrl);
+                document.getElementById("meal_img").setAttribute("src", imageUrl);
+            }
+        };
+        var val = document.getElementById("search").value;
+        myRequest2.open(
+            "POST", "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + val,
+            true
+        );
+        myRequest2.setRequestHeader("Content-Type", "multipart/form-data");
+        myRequest2.setRequestHeader("Ocp-Apim-Subscription-Key", "46fa6f32bc44492d87b34c95e1a37a10");
+        myRequest2.send();
+    }, 1000);
 }
--- test.js
+++ test.js
@@ -1,37 +1,52 @@
 var test = QUnit.test;
 
-test("check if the function `submit` exists",function(t){
+test("check if the function waterfall exists",function(t){
+  t.ok(window.waterfall,"it exists");
+});
+
+test("check if the function submit exists",function(t){
   t.ok(window.submit,"it exists");
 });
 
+test("check if the function getBrands exists",function(t){
+  t.ok(window.getBrands,"it exists");
+});
 
+test("check if the function getImage exists",function(t){
+  t.ok(window.getImage,"it exists");
+});
 
+QUnit.test( "When I click on the submit button it should implement waterfall function", function( t ) {
+  var done = t.async();
+  var done2 = t.async();
+  var done3 = t.async();
+  var done4 = t.async();
+  var input = document.getElementById("btn").onclick = function(){
+        waterfall([
+          submit,
+          getBrands,
+          getImage
+        ]);
+    setTimeout(function() {
+      var val = document.getElementById("brands").textContent;
+      t.equal(typeof(val), "string", "Should insert a sting");
+      done2();
+    },2000);
 
-
-// var res= submit();
-
-// function api_test(url, type, data, callback) {
-//   ajax(
-//     {
-//       url: url,
-//       type: type,
-//       processData: false,
-//       contentType: 'application/json; charset=utf-8',
-//       data: JSON.stringify(data),
-//       dataType: 'json',
-//       async: false,
-//       complete: function (result) {
-//         if (result.status == 0) {
-//           ok(false, '0 status - browser could be on offline mode');
-//         } else if (result.status == 404) {
-//           ok(false, '404 error');
-//         } else {
-//           callback(parseJSON(result.responseText));
-//         }
-//       }
-//     });
-// }
-// test("check if the api return data",function(t){
-
-//   t.ok(api_test(),"Api return");
-// });
+    setTimeout(function(){
+        var calories = document.getElementById('calories').textContent;
+        console.log(calories);
+        var fats = document.getElementById('fats').textContent;
+        console.log(fats);
+        t.equal(calories, "383.03", "When shearch egg and click on Nutritionix brand calories should be 383.03");
+        t.equal(fats, "25.97", "When shearch egg and click on Nutritionix brand fats should be 25.97");
+        done3();
+    },7000);
+    setTimeout(function() {
+      var imageUrl = document.getElementById("meal_img").getAttribute("src");
+      t.equal(imageUrl, "https://tse4.mm.bing.net/th?q=Deviled+Eggs&pid=Api&mkt=en-XA&adlt=strict&t=1", "should return image url");
+      done4();
+    },8000);  
+  };
+  
+});
